3f6b1688c1a65a09e1855742456068102d74f588
ph8nt0m <phant0m@kakao.com>
2022-05-10 20:31:14 +0900

    feat: Add ToDo Func

---
+++ ToDoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by 이동준 on 2022/05/10.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
 
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        let cancleButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(cancleButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    
 }
 
